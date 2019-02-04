c1b2df8a58b97ac3b6df4dda9a95b7e4f5c4e43b
vell_2x <vell690@yahoo.com>
2019-02-04 11:52:35 -0500

    Added HighScore Class

---
+++ src/midterm/HighScore.java
@@ -0,0 +1,25 @@
+package midterm;
+
+public class HighScore {
+	private String name;
+	private int score;
+	
+	public HighScore(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	@Override
+	public String toString() {
+		return name + ", " + score;
+	}
+
+}
